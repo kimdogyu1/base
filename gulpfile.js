39bd3c44e1093257ef0b86ff61267136212a592f
kimdogyu <47593270+kimdogyu1@users.noreply.github.com>
2020-03-23 09:45:42 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -29,3 +29,63 @@ gulp.task('index', function() {
 });
 
 gulp.task('default', ['sass:watch', 'sass']);
+
+
+
+
+
+
+
+
+//////////////////////////pc+ mo
+
+'use stric';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
+var index = require('gulp-index');
+
+gulp.task('clean', function() {
+	return del(['logs/src/*.html'])
+});
+
+gulp.task('sass_pc', function(){
+    return gulp.src('./src/pc/scss/**/*.scss')
+  .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+  .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./src/pc/css'));
+});
+
+gulp.task('sass_pc:watch', function(){
+    gulp.watch('./src/pc/scss/**/*.scss', ['sass_pc']);
+});
+
+gulp.task('index_pc', function() {
+  return gulp.src('./src/pc/*.html')
+    .pipe(index())
+    .pipe(gulp.dest('./src/pc/'));
+});
+
+gulp.task('sass_mobile', function(){
+    return gulp.src('./src/mobile/scss/**/*.scss')
+  .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+  .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./src/mobile/css'));
+});
+
+gulp.task('sass_mobile:watch', function(){
+    gulp.watch('./src/mobile/scss/**/*.scss', ['sass_mobile']);
+});
+
+gulp.task('index_mobile', function() {
+  return gulp.src('./src/mobile/*.html')
+    .pipe(index())
+    .pipe(gulp.dest('./src/mobile/'));
+});
+
+gulp.task('default', ['sass_pc:watch', 'sass_pc','sass_mobile:watch', 'sass_mobile']);
+
